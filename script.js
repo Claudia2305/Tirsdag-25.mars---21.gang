7a9a439a896e4d7a59514a9af4f9539a86cda1dc
Claudia2305 <claudia2305c@hotmail.com>
2025-03-25 11:48:46 +0100

    Oppgave 2

--- script.js
+++ script.js
@@ -8,12 +8,17 @@ async function dataReceived() {
 }
 
 //dataReceived();
-setTimeout(dataReceived, 3000);
+setTimeout(dataReceived, 1000);
+
+console.log("Mens vi venter...");
+console.log("Spis en cupcake...");
+
 
 // Oppgave 1:
 // Sett opp en async funksjon som returnere en string
 // Sett opp en async funksjon som tar imot den første funksjonen 
 // Sett opp en setTimeout som bruker5 sekunderpå å vise dataen 
+//
 
 async function hentData() {
   return "Data er levert!";
@@ -25,4 +30,42 @@ async function dataMottat() {
 }
 
 //dataReceived();
-setTimeout(dataMottat, 1000);
+setTimeout(dataMottat, 2000);
+
+async function displayUser() {
+  try {
+  const user = await new Promise((resolve) => {
+    setTimeout(function () {
+      resolve("Brukerdata er lastet inn!"); 
+    }, 3000);
+    });
+    console.log(user);
+  } catch (error) { 
+      console.log("Error lasting av brukerdata:" + error);
+    }
+  }
+
+displayUser();
+
+// Oppgave 2:
+// Lag en async funksjom som bruker try/catch metoden for å gjennomføre koden
+// Lag en variabel i try som tar imot et løfte som blir løst med en setTimeout
+//Sett opp at catch gir personlig beskjed og en error om det skjer en feil
+
+
+async function hvisBruker() {
+  try {
+  const user = await new Promise((resolve) => {
+    setTimeout(function () {
+      resolve("Brukerdata er lastet inn!"); 
+    }, 3000);
+    });
+    console.log(user);
+  } catch (error) { 
+      console.log("Error lasting av brukerdata:" + error);
+    }
+  }
+
+displayUser();
+
+
