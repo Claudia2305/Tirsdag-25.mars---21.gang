0e4e19988c0cb813211b4becccb8b211571b0927
Claudia2305 <claudia2305c@hotmail.com>
2025-03-25 10:36:04 +0100

    2 sett

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 async function getData() {
   return "Data er hentet!";
 }
+
+async function dataReceived() {
+  const data = await getData();
+  console.log(data);
+}
+
+//dataReceived();
+setTimeout(dataReceived, 3000);
+
+// Oppgave 1:
+// Sett opp en async funksjon som returnere en string
+// Sett opp en async funksjon som tar imot den første funksjonen 
+// Sett opp en setTimeout som bruker5 sekunderpå å vise dataen 
+
+async function hentData() {
+  return "Data er levert!";
+}
+
+async function dataMottat() {
+  const data = await hentData();
+  console.log(data);
+}
+
+//dataReceived();
+setTimeout(dataMottat, 1000);
